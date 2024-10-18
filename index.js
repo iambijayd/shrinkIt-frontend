5a509e832919f5fff7aacd83b8bdb444f5cf2e30
jeewanlamsal <106655205+jeewanlamsal@users.noreply.github.com>
2024-10-18 19:07:50 +0545

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+const shortenBtn=document.getElementById("short-btn");
+const urlInput=document.getElementById("url-input");
+const shortUrlInput=document.getElementById("short-url");
+const copyBtn=document.getElementById("copy-btn");
+
+shortenBtn.addEventListener("click", async function(){
+    const longUrl=urlInput.value;
+
+    //Call the backend API to shorten the URL
+    const response = await fetch("/shorten",{
+        //requesting API to shortened the URL
+        method: "post",   //HTTP request to send data
+        headers:{
+            "content-Type": "application/json"
+        },
+        //Type of content sending from frontend is JSON
+        body:JSON.stringify({url:longUrl})
+        //The data sending to the server is converted into JSON string
+    });
+    const data=await response.json();
+    //waiting for the server response.Once the responce is received,we extract JSON data from response.
+    shortUrlInput.value=data.shortUrl;
+    //Display the shortened URL
+});
+
+copyBtn.addEventListener("click",function(){
+    const shortUrl=shortUrlInput.value;
+    navigator.clipboard.writeText(shortUrl)
+    //The selected url is copied to the clipboard.
+    alert("Short URL copied to clipboard!");
+});
